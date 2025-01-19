6c103add09fc650be1e22b3f9305db407ece4651
AlexE030 <ebner.alexander@gmx.de>
2025-01-19 14:56:31 +0100

    refactor: removed old llms

--- llms/gbert/download_gbert.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-MODEL_NAME = "deepset/gbert-base"
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
-
-print(f"Model {MODEL_NAME} successfully downloaded.")
--- llms/mbart/download_mbart.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-MODEL_NAME = "facebook/mbart-large-50"
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
-
-print(f"Modell {MODEL_NAME} wurde erfolgreich heruntergeladen.")
-

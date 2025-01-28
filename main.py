a741cfc1fa0be7be5b03e92149e90029b50b1265
AlexE030 <ebner.alexander@gmx.de>
2025-01-28 23:26:42 +0100

    connected UI to backend

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import json
+import requests
+
+ROUTER_API_URL = "http://localhost:8080/process/"  # Port 8080 in docker-compose.yml
+
+
+def start_tutor(question):
+    try:
+        # Anfrage an den Router-API-Endpunkt senden
+        response = requests.post(ROUTER_API_URL, json={"text": question})
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Error contacting router_api: {e}"}
+
+
+def main():
+    response = {"message": "error"}
+    try:
+        # Frage aus den Argumenten lesen
+        if len(sys.argv) > 1:
+            question = sys.argv[1]
+        else:
+            question = "Default Question"
+
+        # Anfrage an den Router stellen
+        message_json = start_tutor(question)
+        message = message_json.get("generated_text")
+
+        response.update({"message": message})
+    except Exception as e:
+        response.update({"message": json.dumps(str(e).replace('"', '\\"').replace('\n', '\\n'))})
+    print(json.dumps(response))
+
+
+if __name__ == "__main__":
+    main()

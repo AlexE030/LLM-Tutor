90ebdc11c88afac2dd81c4c5f177ceccf0d7ade5
AlexE030 <ebner.alexander@gmx.de>
2025-02-14 09:15:00 +0100

    removed bitsandbytes

--- models/zephyr-7b/agent_zephyr.py
+++ models/zephyr-7b/agent_zephyr.py
@@ -2,14 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-import bitsandbytes
 
 app = FastAPI()
 
 MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16, load_in_8bit=True, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16, device_map="auto")
 tokenizer.pad_token = tokenizer.eos_token
 
 
@@ -38,7 +37,7 @@ async def generate_outline(input: TextInput):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
-    outputs = model.generate(**inputs, max_new_tokens=100, num_beams=1, early_stopping=True) # TODO: Need to get the reply speed down. Currently aroud 60 sec
+    outputs = model.generate(**inputs, max_new_tokens=100, num_beams=1, early_stopping=True) # TODO: Need to get the reply speed down. Currently around 57 sec
 
     outline = tokenizer.decode(outputs[0], skip_special_tokens=True)
 

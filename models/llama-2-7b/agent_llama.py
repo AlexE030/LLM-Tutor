cf1d86384b859bb302d268dd380dfa376a40b37a
Alexander Ebner <121728015+AlexE030@users.noreply.github.com>
2025-02-21 19:00:08 +0100

    Zephyr integration (#22)
    * Initial llama integration
    
    * added llama container
    
    * Added Llama for Testing on Server
    
    * Fixed Padding
    
    * Testing GPU usage
    
    * Added cuda
    
    * Docker Image fix
    
    * Pull cuda Image
    
    * Install python in Container
    
    * fix?
    
    * Changed Image to devel
    
    * Add venv to container
    
    * Potential fix with installation of GLIBC 2.39
    
    * Adjusted docker-compose.yml
    
    * Load input_ids on GPU
    
    * removed comments
    
    * Basic zephyr integration
    
    * Optimize VRAM usage
    
    * Device Map
    
    * further optimization
    
    * further optimization
    
    * further optimization
    
    * removed bitsandbytes

---
+++ models/llama-2-7b/agent_llama.py
@@ -0,0 +1,46 @@
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+app = FastAPI()
+
+MODEL_NAME = "meta-llama/Llama-2-7b-chat-hf"
+HF_TOKEN = os.environ.get("HF_TOKEN", None)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, token=HF_TOKEN)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, token=HF_TOKEN, torch_dtype=torch.bfloat16, device_map="auto")
+tokenizer.pad_token = tokenizer.eos_token
+
+
+class TextInput(BaseModel):
+    text: str
+
+
+@app.on_event("startup")
+def load_model():
+    global model, tokenizer
+    model.eval()
+
+
+@app.post("/process/")
+async def generate_outline(input: TextInput):
+    prompt = (
+        f"Du bist ein Experte für die Gliederung von Wissenschaftlichen Arbeiten."
+        f"Deine Aufgabe ist es eine Gliederung zu einer Wissenschaftlichen Arbeit zu schreiben."
+        f"Bitte nenne nur die Gliederung ohne weitere Erläuterungen"
+        f"Im Folgenden erhälst du weitere Informationen zum Thema\n\n"
+        f"Benutzereingabe: {input.text}\n"
+    )
+
+    print(prompt)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
+    outputs = model.generate(**inputs, max_length=500, num_beams=5, early_stopping=True)
+
+    outline = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return {"gliederung": outline}
---
+++ models/zephyr-7b/agent_zephyr.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+app = FastAPI()
+
+MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16, device_map="auto")
+tokenizer.pad_token = tokenizer.eos_token
+
+
+class TextInput(BaseModel):
+    text: str
+
+
+@app.on_event("startup")
+def load_model():
+    global model, tokenizer
+    model.eval()
+
+
+@app.post("/process/")
+async def generate_outline(input: TextInput):
+    prompt = (
+        f"Du bist ein Experte für das erstellen von Zitaten in Wissenschaftlichen Arbeiten."
+        f"Deine Aufgabe ist es ein Zitat nach einer definierten Zitationsart zu erstellen."
+        f"Bitte gebe nur das Zitat aus, ohne weitere Erläuterungen"
+        f"Hier hast du die Eingabe des Benutzers\n\n"
+        f"Benutzereingabe: {input.text}\n"
+    ) # TODO: This is not a proper prompt, but just for Testing for basic integration of Zephyr
+
+    print(prompt)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
+    outputs = model.generate(**inputs, max_new_tokens=100, num_beams=1, early_stopping=True) # TODO: Need to get the reply speed down. Currently around 57 sec
+
+    outline = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return {"citation": outline}

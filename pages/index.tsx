592ce16679c4f11676b99c60f80c4496c3dbd22d
AlexE030 <ebner.alexander@gmx.de>
2025-03-23 15:09:27 +0100

    removed grammar mistake

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [loading, setLoading] = useState<boolean>(false);
   const [initializing, setInitializing] = useState<boolean>(true);
   const [errorMessage, setErrorMessage] = useState<string>('');
-  const chatbotContent = `Sag mir zuerst, **WAS** ich tun soll (Aktion), dann **WOMIT* (Text), getrennt durch einen Doppelpunkt \nBeispiel: Erstelle mir ein Zitat von: ...`;
+  const chatbotContent = `Herzlich willkommen beim Assistenten für Bachelorarbeiten\nUm einen Reibungslosen Ablauf zu gewährleisten sag mir zuerst, WAS ich tun soll (Aktion), dann WOMIT (Text), getrennt durch einen Doppelpunkt \nBeispiel: Erstelle mir ein Gliederung zu: ...`;
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
